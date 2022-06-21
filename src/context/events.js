b8b9ecc20448f2ad2034e486f4b008b5614bcd85
Furkan Pamuk <furkanpamuk-@hotmail.com>
2022-06-21 21:18:02 +0300

    initial created

---
+++ src/context/events.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+import React, { useContext, createContext, useState, useEffect } from "react";
+
+const EventsContext = createContext();
+
+const EventsProvider = ({ children }) => {
+    const [events, setEvents] = useState({})
+    const [selectEvent, setSelectEvent] = useState({})
+
+    const getEvents = async () => {
+        const data = await axios("https://etstur-804bc-default-rtdb.firebaseio.com/.json")
+        console.log(data.data);
+        setEvents(data.data)
+    }
+
+    useEffect(() => {
+        getEvents()
+    }, [])
+
+    const data = {
+        events,
+        setEvents,
+
+        selectEvent,
+        setSelectEvent
+    }
+
+    return (
+        <EventsContext.Provider value={data}>
+            {children}
+        </EventsContext.Provider>
+    )
+}
+
+function useEvents() {
+    return useContext(EventsContext);
+}
+
+export { EventsProvider, EventsContext, useEvents }
